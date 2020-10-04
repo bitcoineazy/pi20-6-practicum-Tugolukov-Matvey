17de77cc9af73d2488bcee0db84a0e59688cd4a4
bitcoineazy <70742214+bitcoineazy@users.noreply.github.com>
2020-10-04 22:54:37 +0300

    Final

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ width = 5
 height = 4
 table = [['*'] * width for i in range(height)]
 
-def draw_board(a: list):
+def draw_board(a):
     print("-" * (len(table) * 5 + 1))
     for i in range(len(a)):
         print("|", end="")
@@ -15,10 +15,6 @@ print('"Лоскутное одеяло" игра для 3-х игроков')
 draw_board(table)
 print('Введите координаты хода в через пробел формате: НОМЕР_В_СТРОКЕ НОМЕР_СТОЛБЦА \nШтрафные очки подсчитываются в конце игры')
 
-dxy = [[-1, -1], [-1, 0], [-1, 1], \
-       [0,  -1],          [0,  1], \
-       [1,  -1], [1,  0], [1,  1]  ]
-score = [0, 0, 0]
 
 
 current_move = 0
@@ -41,19 +37,23 @@ while current_move < 20:
         draw_board(table)
         current_move += 1
 
+#это дельты соответственных координат по различным направлениям в одномерном массиве пар
+#для каждого направления прописал, какие координаты у клетки-соседа в этом направлении относительно изначальной.
+dxy = [[-1, -1], [-1, 0], [-1, 1],
+       [0,  -1],          [0,  1],
+       [1,  -1], [1,  0], [1,  1]  ]
+score = [0, 0, 0]
 
-
-for y in range(4):
-    for x in range(5):
+for y in range(height):
+    for x in range(width):
         for i in range(len(dxy)):
-            nx, ny = x + dxy[i][0], y + dxy[i][1]
+            nx, ny = x + dxy[i][0], y + dxy[i][1] #nx, ny - это координаты клетки-соседа в i-том направлении
             if 0 <= nx < 5 and 0 <= ny < 4:
                 if table[y][x] == table[ny][nx]:
                     try:
                         score[int(table[y][x]) - 1] += 1
-                        print(score)
                     except ValueError:
-                        print('Что-то определённо не так(нельзя ставить символы уже на заполненные места)')
+                        print('Что-то определённо не так')
                         continue
 
 #Теперь в score удвоенное количество штрафных очков каждого игрока
@@ -62,11 +62,11 @@ score = [i // 2 for i in score]
 for i in range(len(players_char)):
     print(f'Игрок {i + 1} получил {score[i]} штрафных очков')
 
-if score[0] < (score[1] and score[2]):
+if score[0] < score[1] and score[0] < score[2]:
     print(f'Победил 1-ый игрок')
-elif score[1] < (score[0] and score[2]):
+elif score[1] < score[0] and score[1] < score[2]:
     print(f'Победил 2-ой игрок')
-elif score[2] < (score[0] and score[1]):
+elif score[2] < score[0] and score[2] < score[1]:
     print(f'Победил 3-ий игрок')
 elif (score[0] == score[1]) and score[0] < score[2] and score[1] < score[2]:
     print(f'Победили два игрока 1 и 2')
